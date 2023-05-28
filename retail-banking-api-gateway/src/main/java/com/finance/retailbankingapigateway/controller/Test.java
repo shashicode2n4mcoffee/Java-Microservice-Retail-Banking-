f1c35793ec6290602f1a8a1a5c614fdb48716470
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-05-28 15:32:13 +0530

    Added the reatil core banking as route in api gateway

--- retail-banking-api-gateway/src/main/java/com/finance/retailbankingapigateway/controller/Test.java
+++ retail-banking-api-gateway/src/main/java/com/finance/retailbankingapigateway/controller/Test.java
@@ -1,2 +1,15 @@
-package com.finance.retailbankingapigateway.controller;public class Test {
+package com.finance.retailbankingapigateway.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping
+    public ResponseEntity testApi (){
+        return ResponseEntity.ok("API Gateway is working");
+    }
 }
